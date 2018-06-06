7e742c10b3b3040410ff10a5fa8329f7f79c2ba2
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-06 19:48:47 +0530

    code checking

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 
 
-<?php header('Access-Control-Allow-Origin: *');?>
-<?php header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');?>
+<?php header("Access-Control-Allow-Origin: *");?>
+<?php header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");?>
 <?php header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization ,X-Auth-Token");?>
 <?php header( 'Location: /index.html') ;?>
 
