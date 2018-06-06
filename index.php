c9050c521600ea0c3912dadb02c329b697090afc
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-06 18:29:11 +0530

    code change php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 
 
 
-<?php header('Access-Control-Allow-Origin: https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv');?>
-<?php header('Access-Control-Allow-Methods: GET, POST');?>
-<?php header("Access-Control-Allow-Headers: X-Requested-With");?>
+<?php header('Access-Control-Allow-Origin: *');?>
+<?php header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');?>
+<?php header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization ,X-Auth-Token");?>
 <?php header( 'Location: /index.html') ;?>
 
 
-
-	
-	
-	
 	
 	
 	
