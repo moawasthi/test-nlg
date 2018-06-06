88047879d0cff47fa2ab3f682744646a50608a11
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-06 21:14:19 +0530

    php code change

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <?php header("Access-Control-Allow-Origin: *");?>
 <?php header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");?>
 <?php header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization ,X-Auth-Token");?>
-<?php header( 'Location: /index.html') ;?>
-
 <html>
 <head>
     <title>jQuery AJAX POST Form</title>
