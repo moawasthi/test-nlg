da589d35094299e3d52445a0102f30c487ecddf4
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-06 21:23:11 +0530

    code change nlg

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 
 
 
-<?php header("Access-Control-Allow-Origin: *");?>
-<?php header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");?>
-<?php header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization ,X-Auth-Token");?>
+<?php header('Access-Control-Allow-Origin: *');?>
+<?php header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');?>
+<?php header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization ,X-Auth-Token');?>
 <html>
 <head>
     <title>jQuery AJAX POST Form</title>
