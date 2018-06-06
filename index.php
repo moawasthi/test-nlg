73884fa5f1b617b400ded12a86397095b4f5481d
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-06 18:11:45 +0530

    test nlg

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 
 
-<?php header('Access-Control-Allow-Origin: https://app.studio.arria.com/alite_content_generation_webapp/text/ExnK8bQwJYv');?>
+<?php header('Access-Control-Allow-Origin: https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv');?>
 <?php header('Access-Control-Allow-Methods: GET, POST');?>
 <?php header("Access-Control-Allow-Headers: X-Requested-With");?>
 <?php header( 'Location: /index.html') ;?>
