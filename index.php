79d2616a8e50982f3e2e908ccb47388323ce64f3
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-08 16:33:24 +0530

    nlg code change

--- index.php
+++ index.php
@@ -1,200 +1,143 @@
 
 
 
-<?php header('Access-Control-Allow-Origin: *');?>
-<?php header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');?>
-<?php header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization ,X-Auth-Token');?>
-<?php 
-  $url = "https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv";
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "content-type: application/json;charset=UTF-8" ,
-"Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJyRDY2VnF6cGRLQzVOaXh6a2JPWUttQlMiLCJpYXQiOjE1MjgzNjAzMjIsImV4cCI6MTY4NjA0MDMyMiwiaXNzIjoiQUxpdGUiLCJzdWIiOiI3RlRpQzJPdUlha20iLCJBTGl0ZS5wZXJtIjpbInByczp4OkV4bks4YlF3Sll2Il0sIkFMaXRlLnR0IjoidV9hIn0.TYyppdHz6w3HEikX4KgJn9AKWVnWtEIa6VGnQlP6GzUYvI39ONwTQ1sMCFrQWSuGqzAsVWvCzC7X_gDIFLUkRw"));
-curl_setopt($ch, CURLOPT_POSTFIELDS, 
-"{
-'data':[
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_PORT => "443",
+  CURLOPT_URL => "https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJyRDY2VnF6cGRLQzVOaXh6a2JPWUttQlMiLCJpYXQiOjE1MjgzNjAzMjIsImV4cCI6MTY4NjA0MDMyMiwiaXNzIjoiQUxpdGUiLCJzdWIiOiI3RlRpQzJPdUlha20iLCJBTGl0ZS5wZXJtIjpbInByczp4OkV4bks4YlF3Sll2Il0sIkFMaXRlLnR0IjoidV9hIn0.TYyppdHz6w3HEikX4KgJn9AKWVnWtEIa6VGnQlP6GzUYvI39ONwTQ1sMCFrQWSuGqzAsVWvCzC7X_gDIFLUkRw",
+    "Cache-Control: no-cache",
+    "Content-Type: application/json;charset=UTF-8",
+    "Postman-Token: 74fd0d0f-6382-4e09-b68b-6368545d4d03"
+  ),
+));
+$body = <<<EOT
+{
+  "data":[
     {
-      'id':'Primary',
-      'type':'json',
-      'jsonData':
+      "id":"Primary",
+      "type":"json",
+      "jsonData":
       {
-        {
-          'month': 'August',
-          'year': '2017',
-          'total': 571675,
-          'target': 500000,
-          'states': [
-            'New York',
-            'California',
-            'Texas'
+        
+          "month": "August",
+          "year": "2017",
+          "total": 571675,
+          "target": 500000,
+          "states": [
+            "New York",
+            "California",
+            "Texas"
           ],
-          'offices': [
+          "offices": [
             {
-              'state': 'California',
-              'name': 'L.A.',
-              'sales': 85320,
-              'target': 75000,
-              'manager': {
-                'firstName': 'Frank',
-                'surname': 'Langford',
-                'yearsEmployed': 1,
-                'role': 'Independent Contractor'
+              "state": "California",
+              "name": "L.A.",
+              "sales": 85320,
+              "target": 75000,
+              "manager": {
+                "firstName": "Frank",
+                "surname": "Langford",
+                "yearsEmployed": 1,
+                "role": "Independent Contractor"
               },
-              'employees': 3
+              "employees": 3
             },
             {
-              'state': 'Texas',
-              'name': 'Houston',
-              'sales': 70530,
-              'target': 75000,
-              'manager': {
-                'firstName': 'Maria',
-                'surname': 'Oliviera',
-                'yearsEmployed': 4,
-                'role': 'Sales Manager'
+              "state": "Texas",
+              "name": "Houston",
+              "sales": 70530,
+              "target": 75000,
+              "manager": {
+                "firstName": "Maria",
+                "surname": "Oliviera",
+                "yearsEmployed": 4,
+                "role": "Sales Manager"
               },
-              'employees': 1
+              "employees": 1
             },
             {
-              'state': 'New York',
-              'name': 'New York',
-              'sales': 280425,
-              'target': 200000,
-              'manager': {
-                'firstName': 'Nikhil',
-                'surname': 'Patel',
-                'yearsEmployed': 2,
-                'role': 'Senior Sales Manager'
+              "state": "New York",
+              "name": "New York",
+              "sales": 280425,
+              "target": 200000,
+              "manager": {
+                "firstName": "Nikhil",
+                "surname": "Patel",
+                "yearsEmployed": 2,
+                "role": "Senior Sales Manager"
               },
-              'employees': 8
+              "employees": 8
             },
             {
-              'state': 'Texas',
-              'name': 'Dallas',
-              'sales': 45090,
-              'target': 50000,
-              'manager': {
-                'firstName': 'David',
-                'surname': 'McGruer',
-                'yearsEmployed': 0,
-                'role': 'Sales Manager'
+              "state": "Texas",
+              "name": "Dallas",
+              "sales": 45090,
+              "target": 50000,
+              "manager": {
+                "firstName": "David",
+                "surname": "McGruer",
+                "yearsEmployed": 0,
+                "role": "Sales Manager"
               },
-              'employees': 1
+              "employees": 1
             },
             {
-              'state': 'California',
-              'name': 'San Francisco',
-              'sales': 29450,
-              'target': 50000,
-              'manager': {
-                'firstName': 'Diana',
-                'surname': 'Jones',
-                'yearsEmployed': 0,
-                'role': 'Independent Contractor'
+              "state": "California",
+              "name": "San Francisco",
+              "sales": 29450,
+              "target": 50000,
+              "manager": {
+                "firstName": "Diana",
+                "surname": "Jones",
+                "yearsEmployed": 0,
+                "role": "Independent Contractor"
               },
-              'employees': 1
+              "employees": 1
             },
             {
-              'state': 'Texas',
-              'name': 'Austin',
-              'sales': 60860,
-              'target': 50000,
-              'manager': {
-                'firstName': 'Ava',
-                'surname': 'Peake',
-                'yearsEmployed': 2,
-                'role': 'Sales Manager'
+              "state": "Texas",
+              "name": "Austin",
+              "sales": 60860,
+              "target": 50000,
+              "manager": {
+                "firstName": "Ava",
+                "surname": "Peake",
+                "yearsEmployed": 2,
+                "role": "Sales Manager"
               },
-              'employees': 2
+              "employees": 2
             }
           ]
         }
-}"
-
-); 
-$repsonse = curl_exec($ch); 
-  $result = json_decode($repsonse);
-  echo $result;
-?>
-
-<?php 
-//setup request to send json via POST
-$url = "https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv";
-  $ch = curl_init($url);
-
-
-//attach encoded JSON string to the POST fields
-
-//set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json;charset=UTF-8" ,
-"Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJyRDY2VnF6cGRLQzVOaXh6a2JPWUttQlMiLCJpYXQiOjE1MjgzNjAzMjIsImV4cCI6MTY4NjA0MDMyMiwiaXNzIjoiQUxpdGUiLCJzdWIiOiI3RlRpQzJPdUlha20iLCJBTGl0ZS5wZXJtIjpbInByczp4OkV4bks4YlF3Sll2Il0sIkFMaXRlLnR0IjoidV9hIn0.TYyppdHz6w3HEikX4KgJn9AKWVnWtEIa6VGnQlP6GzUYvI39ONwTQ1sMCFrQWSuGqzAsVWvCzC7X_gDIFLUkRw"));
-$repsonse = curl_exec($ch); 
-echo $repsonse;
-$result = json_decode($repsonse);
-echo $result;
-//close cURL resource
-curl_close($ch);
-//return response instead of outputting
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-//execute the POST request
-$repsonse = curl_exec($ch); 
-echo $repsonse;
-$result = json_decode($repsonse);
-echo $result;
-//close cURL resource
-curl_close($ch);
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-    $("button").click(function(){
-      var url = 'https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv';
-      
-      makePostCall = function (url) { // here the data and url are not hardcoded anymore
-   var json_data = JSON.stringify("" );
-
-    return $.ajax({
-        type: "POST",
-        url: url,
-        data: json_data,
-        dataType: "jsonp",
-        contentType: "application/json;charset=UTF-8",
-        authentication: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJyRDY2VnF6cGRLQzVOaXh6a2JPWUttQlMiLCJpYXQiOjE1MjgzNjAzMjIsImV4cCI6MTY4NjA0MDMyMiwiaXNzIjoiQUxpdGUiLCJzdWIiOiI3RlRpQzJPdUlha20iLCJBTGl0ZS5wZXJtIjpbInByczp4OkV4bks4YlF3Sll2Il0sIkFMaXRlLnR0IjoidV9hIn0.TYyppdHz6w3HEikX4KgJn9AKWVnWtEIa6VGnQlP6GzUYvI39ONwTQ1sMCFrQWSuGqzAsVWvCzC7X_gDIFLUkRw"
-    });
+		}
+    
+	
+  ]
 }
+EOT;
+curl_setopt($curl, CURLOPT_POSTFIELDS, $body); 
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-// and here a call example
-makePostCall("https://app.studio.arria.com:443/alite_content_generation_webapp/text/ExnK8bQwJYv")
-    .success(function(data){
-               console.log(data);
-   })
-    .fail(function(sender, message, details){
-           alert("Sorry, something went wrong!");
-  });
-       
-    });
-});
-</script>
-</head>
-<body>
+curl_close($curl);
 
-<button>Send an HTTP POST request to a page and get the result back</button>
-<?php echo($resp); ?>
-<?php
-echo "Hello world!";
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo var_dump($response);
+}
 ?>
-</body>
-</html>
-	
-	
-	
-	
-	
-	
+
+
 
