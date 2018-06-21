4e88729defc44e8814bd3def29b7a874824f490f
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-21 07:05:04 +0530

    nlg

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $curl = curl_init();
-$command= escapeshellcmd('sudo -u www -data derive_stats.py');
+$command= escapeshellcmd('python derive_stats.py');
 $output = shell_exec($command);
 $resultArray = json_decode($output);
 echo $result;
