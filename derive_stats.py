125bdea6255fdef9c4172b36b8448e76e6f05635
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-23 17:42:20 +0530

    nlg

--- derive_stats.py
+++ derive_stats.py
@@ -14,33 +14,6 @@ class Derive_Stats:
     list_of_sub_category=[]
     list_of_sum_profit=[]
     list_actual=[]
-    """
-    def return_output(self,x):
-        json_data = json.loads(base64.b64decode(sys.argv[1]))
-        for i in json_data:
-            self.list_of_sub_category.append(i.get('Sub-Category'))
-            self.list_actual.append(i.get('SUM(Profit)'))
-            if not isinstance(i.get('SUM(Profit)'), int): 
-            
-                self.list_of_sum_profit.append(int(i.get('SUM(Profit)').replace('$','').replace(',','')))
-            else :
-                self.list_of_sum_profit.append(int(i.get('SUM(Profit)')))
-                max1=max(self.list_of_sum_profit)
-                min1=min(self.list_of_sum_profit)
-                median1=np.median(self.list_of_sum_profit)
-        
-        #output=[{self.list_of_sub_category[self.list_of_sum_profit.index(max1)]:self.list_actual[self.list_of_sum_profit.index(max1)],
-         #       self.list_of_sub_category[self.list_of_sum_profit.index(min1)]: self.list_actual[self.list_of_sum_profit.index(min1)],
-          #      'median':median1}]
-        
-
-        output={"dashboards":[{"name": "Sales","KPI": {"first":self.list_of_sub_category[self.list_of_sum_profit.index(max1)],"second":self.list_of_sub_category[self.list_of_sum_profit.index(min1)],"third":"median"},"Measures" :{"first" :self.list_actual[self.list_of_sum_profit.index(max1)],"second" :self.list_actual[self.list_of_sum_profit.index(min1)],"third" : median1}}]}
-        #self.output_file = open(self.output_file, "wb", encoding="utf8")
-        #self.output_file.write(json.dumps(output))
-        s=json.dumps(output)
-        return s
-        
-"""
     def stat_result(self,input,output):
         self.input_file=open(input,'r')
         self.output_file=open(output,'wb')
@@ -82,8 +55,3 @@ class Derive_Stats:
 
 test=Derive_Stats()
 print (test.stat_result(r'Input_Sample1.json',r'output.json'))
-"""
-x = sys.argv[1]
-test=Derive_Stats()
-print (test.return_output(x))
-"""
