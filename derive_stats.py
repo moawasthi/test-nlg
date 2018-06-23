55b5ecde660fbdf2870abb11f9decba110d411d3
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-06-23 16:55:09 +0530

    nlg

--- derive_stats.py
+++ derive_stats.py
@@ -23,21 +23,21 @@ class Derive_Stats:
             
                 self.list_of_sum_profit.append(int(i.get('SUM(Profit)').replace('$','').replace(',','')))
             else :
-                self.list_of_sum_profit.append(int(i.get('SUM(Profit)')
+                self.list_of_sum_profit.append(int(i.get('SUM(Profit)'))
                 max1=max(self.list_of_sum_profit)
                 min1=min(self.list_of_sum_profit)
                 median1=np.median(self.list_of_sum_profit)
         
-        output=[{self.list_of_sub_category[self.list_of_sum_profit.index(max1)]:self.list_actual[self.list_of_sum_profit.index(max1)],
-                self.list_of_sub_category[self.list_of_sum_profit.index(min1)]: self.list_actual[self.list_of_sum_profit.index(min1)],
-                'median':median1}]
+        #output=[{self.list_of_sub_category[self.list_of_sum_profit.index(max1)]:self.list_actual[self.list_of_sum_profit.index(max1)],
+         #       self.list_of_sub_category[self.list_of_sum_profit.index(min1)]: self.list_actual[self.list_of_sum_profit.index(min1)],
+          #      'median':median1}]
         
 
         output={"dashboards":[{"name": "Sales","KPI": {"first":self.list_of_sub_category[self.list_of_sum_profit.index(max1)],"second":self.list_of_sub_category[self.list_of_sum_profit.index(min1)],"third":"median"},"Measures" :{"first" :self.list_actual[self.list_of_sum_profit.index(max1)],"second" :self.list_actual[self.list_of_sum_profit.index(min1)],"third" : median1}}]}
         #self.output_file = open(self.output_file, "wb", encoding="utf8")
         #self.output_file.write(json.dumps(output))
-
-        return output
+        s=json.dumps(output)
+        return s
         
 """
     def stat_result(self,input,output):
